94fe2b7baff5f8e4d12bd2cb3c89fb6330d799bb
Przemysław Postrach <przemyslaw.postrach@hotmail.com>
2021-12-17 09:48:43 +0100

    Add staticpages app and fix RWD

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
-    path('uslugi', views.Services.as_view(), name='services'),
-    path('cennik', views.Prices.as_view(), name='prices'),
-    path('floty', views.Fleets.as_view(), name='fleets'),
-    path('kontakt', views.Contact.as_view(), name='contact'),
-    path('onas', views.About.as_view(), name='about'),
     path('aktualnosci', views.PostList.as_view(), name='news'),
     path('aktualnosci/<slug:slug>/', views.PostDetail.as_view(), name='news_detail')
 ]
---
+++ staticpages/views.py
@@ -0,0 +1,28 @@
+from django.views import generic
+
+# Create your views here.
+
+
+class Index(generic.TemplateView):
+    template_name = 'index.html'
+
+
+class Services(generic.TemplateView):
+    template_name = 'services.html'
+
+
+class Prices(generic.TemplateView):
+    template_name = 'prices.html'
+
+
+class Fleets(generic.TemplateView):
+    template_name = 'fleets.html'
+
+
+class About(generic.TemplateView):
+    template_name = 'about.html'
+
+
+class Contact(generic.TemplateView):
+    template_name = 'contact.html'
+
